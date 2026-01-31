7946e48337848078c909c010b51191ebbe86b5e4
detox267 <shanaman07@hotmail.com>
2026-01-31 20:18:50 +1100

    working

--- programs/aaped-launch/src/math.rs
+++ programs/aaped-launch/src/math.rs
@@ -7,18 +7,8 @@ pub const TOKEN_DECIMALS: u128 = 1_000_000;
 pub const V_SOL: u128 = 30 * LAMPORTS_PER_SOL;
 pub const V_TOK: u128 = 526_200_000 * TOKEN_DECIMALS;
 
-pub const SALE_TOTAL: u128 = 600_000_000 * TOKEN_DECIMALS;
-pub const TAIL_START_REMAIN: u128 = 15_000_000 * TOKEN_DECIMALS;
-pub const TAIL_END_REMAIN: u128 = 5_000_000 * TOKEN_DECIMALS;
-
 pub const MIGRATION_SOL_TARGET: u128 = 85 * LAMPORTS_PER_SOL;
-pub const LP_TOTAL: u128 = 400_000_000u128 * TOKEN_DECIMALS;
-
-// Fees (bps)
-pub const FEE_TOTAL_BPS: u128 = 125;
-pub const FEE_CREATOR_BPS: u128 = 80;
-pub const FEE_PLATFORM_BPS: u128 = 20;
-pub const FEE_LP_GROWTH_BPS: u128 = 25;
+pub const LP_TOTAL: u128 = 400_000_000 * TOKEN_DECIMALS;
 
 #[inline]
 pub fn bps_amount(amount: u128, bps: u128) -> Result<u128> {
@@ -27,9 +17,18 @@ pub fn bps_amount(amount: u128, bps: u128) -> Result<u128> {
     Ok(div)
 }
 
-/// Buy on curve
-pub fn curve_buy(sol_in: u128, sol_real: u128, tok_real: u128) -> Result<(u128, u128, u128)> {
-    let fee_total = bps_amount(sol_in, FEE_TOTAL_BPS)?;
+/// Split fees: base + LP growth
+pub fn split_trade_fee(sol_in: u128, base_bps: u128, lp_bps: u128) -> Result<(u128, u128)> {
+    let base = bps_amount(sol_in, base_bps)?;
+    let lp = bps_amount(sol_in, lp_bps)?;
+    Ok((base, lp))
+}
+
+/// Constant product curve buy
+pub fn curve_buy(sol_in: u128, sol_real: u128, tok_real: u128, fee_bps: u128)
+-> Result<(u128, u128, u128)>
+{
+    let fee_total = bps_amount(sol_in, fee_bps)?;
     let sol_eff = sol_in.checked_sub(fee_total).ok_or(error!(AapedError::MathOverflow))?;
 
     let r_sol = V_SOL.checked_add(sol_real).ok_or(error!(AapedError::MathOverflow))?;
@@ -45,24 +44,31 @@ pub fn curve_buy(sol_in: u128, sol_real: u128, tok_real: u128) -> Result<(u128, 
     Ok((tokens_out, sol_eff, fee_total))
 }
 
-/// Sell on curve
-pub fn curve_sell(tokens_in: u128, sol_real: u128, tok_real: u128) -> Result<(u128, u128, u128)> {
+/// Optional sell math
+pub fn curve_sell(tokens_in: u128, sol_real: u128, tok_real: u128, fee_bps: u128)
+-> Result<(u128, u128, u128)>
+{
     let r_sol = V_SOL.checked_add(sol_real).ok_or(error!(AapedError::MathOverflow))?;
     let r_tok = V_TOK.checked_add(tok_real).ok_or(error!(AapedError::MathOverflow))?;
+
     let k = r_sol.checked_mul(r_tok).ok_or(error!(AapedError::MathOverflow))?;
 
     let r_tok_new = r_tok.checked_add(tokens_in).ok_or(error!(AapedError::MathOverflow))?;
     let r_sol_new = k.checked_div(r_tok_new).ok_or(error!(AapedError::MathOverflow))?;
 
     let sol_gross = r_sol.checked_sub(r_sol_new).ok_or(error!(AapedError::MathOverflow))?;
-    let fee_total = bps_amount(sol_gross, FEE_TOTAL_BPS)?;
+
+    let fee_total = bps_amount(sol_gross, fee_bps)?;
     let sol_net = sol_gross.checked_sub(fee_total).ok_or(error!(AapedError::MathOverflow))?;
 
     Ok((sol_gross, fee_total, sol_net))
 }
 
-pub fn tail_buy(sol_in: u128) -> Result<(u128, u128, u128)> {
-    let fee_total = bps_amount(sol_in, FEE_TOTAL_BPS)?;
+/// Tail pricing
+pub fn tail_buy(sol_in: u128, fee_bps: u128)
+-> Result<(u128, u128, u128)>
+{
+    let fee_total = bps_amount(sol_in, fee_bps)?;
     let sol_eff = sol_in.checked_sub(fee_total).ok_or(error!(AapedError::MathOverflow))?;
 
     let tokens_out = sol_eff
@@ -73,3 +79,4 @@ pub fn tail_buy(sol_in: u128) -> Result<(u128, u128, u128)> {
 
     Ok((tokens_out, sol_eff, fee_total))
 }
+
--- programs/aaped-launch/src/state.rs
+++ programs/aaped-launch/src/state.rs
@@ -11,83 +11,22 @@ pub enum LaunchPhase {
 #[account]
 pub struct LaunchState {
     pub bump: u8,
-
     pub state: u8,
 
     pub mint: Pubkey,
     pub creator: Pubkey,
     pub platform: Pubkey,
 
-    // vaults
+    // token vaults
     pub sale_vault: Pubkey,
     pub lp_vault: Pubkey,
 
-    // SOL vaults (system accounts owned by program PDA)
+    // SOL vaults (system accounts owned by PDA)
     pub treasury_sol_vault: Pubkey,
     pub creator_sol_vault: Pubkey,
     pub platform_sol_vault: Pubkey,
 
-    // tokenomics (all in smallest units, decimals already applied)
-    pub total_supply: u64,
-    pub sale_supply: u64,
-    pub lp_supply: u64,
-
-    // curve config (fixed-point basis, keep as u64 for now; math uses u128)
-    pub v_sol: u64, // lamports virtual
-    pub v_tok: u64, // token units virtual
-
-    // tail thresholds (token units remaining in sale vault)
-    pub tail_start: u64,
-    pub tail_end: u64,
-
-    // migration conditions
-    pub migration_sol_target: u64, // lamports target
-
-    // fees in basis points
-    pub fee_total_bps: u16,
-    pub fee_creator_bps: u16,
-    pub fee_platform_bps: u16,
-    pub fee_lp_growth_bps: u16,
-
-    // accounting
-    pub tokens_sold: u64,
-    pub sol_collected: u128, // store in u128 to avoid overflow
-}
-
-impl LaunchState {
-    pub const LEN: usize =
-        8  // disc
-        + 1  // bump
-        + 1  // state
-        + 32 // mint
-        + 32 // creator
-        + 32 // platform
-        + 32 // sale_vault
-        + 32 // lp_vault
-        + 32 // treasury_sol_vault
-        + 32 // creator_sol_vault
-        + 32 // platform_sol_vault
-        + 8  // total_supply
-        + 8  // sale_supply
-        + 8  // lp_supply
-        + 8  // v_sol
-        + 8  // v_tok
-        + 8  // tail_start
-        + 8  // tail_end
-        + 8  // migration_sol_target
-        + 2  // fee_total_bps
-        + 2  // fee_creator_bps
-        + 2  // fee_platform_bps
-        + 2  // fee_lp_growth_bps
-        + 8  // tokens_sold
-        + 16; // sol_collected
-}
-
-#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
-pub struct InitializeParams {
-    pub creator: Pubkey,
-    pub platform: Pubkey,
-
+    // tokenomics
     pub total_supply: u64,
     pub sale_supply: u64,
     pub lp_supply: u64,
@@ -100,6 +39,7 @@ pub struct InitializeParams {
     pub tail_start: u64,
     pub tail_end: u64,
 
+    // migration target
     pub migration_sol_target: u64,
 
     // fees
@@ -107,4 +47,69 @@ pub struct InitializeParams {
     pub fee_creator_bps: u16,
     pub fee_platform_bps: u16,
     pub fee_lp_growth_bps: u16,
+
+    // accounting
+    pub tokens_sold: u64,
+    pub sol_collected: u128,
+    pub lp_growth_sol: u128,
+
+    // timing
+    pub launch_ts: i64,
+    pub last_trade_ts: i64,
 }
+
+impl LaunchState {
+    pub const LEN: usize =
+        8  // discriminator
+        + 1  // bump
+        + 1  // state
+        + 32 // mint
+        + 32 // creator
+        + 32 // platform
+        + 32 // sale_vault
+        + 32 // lp_vault
+        + 32 // treasury
+        + 32 // creator sol
+        + 32 // platform sol
+        + 8  // total_supply
+        + 8  // sale_supply
+        + 8  // lp_supply
+        + 8  // v_sol
+        + 8  // v_tok
+        + 8  // tail_start
+        + 8  // tail_end
+        + 8  // migration target
+        + 2  // fee_total
+        + 2  // fee_creator
+        + 2  // fee_platform
+        + 2  // fee_lp_growth
+        + 8  // tokens_sold
+        + 16 // sol_collected
+        + 16 // lp_growth_sol
+        + 8  // launch_ts
+        + 8; // last_trade_ts
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct InitializeParams {
+    pub creator: Pubkey,
+    pub platform: Pubkey,
+
+    pub total_supply: u64,
+    pub sale_supply: u64,
+    pub lp_supply: u64,
+
+    pub v_sol: u64,
+    pub v_tok: u64,
+
+    pub tail_start: u64,
+    pub tail_end: u64,
+
+    pub migration_sol_target: u64,
+
+    pub fee_total_bps: u16,
+    pub fee_creator_bps: u16,
+    pub fee_platform_bps: u16,
+    pub fee_lp_growth_bps: u16,
+}
+
