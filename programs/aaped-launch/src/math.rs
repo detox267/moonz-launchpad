e5f76dfdc4e03414424d6d4a2358f4c178d9268c
detox267 <shanaman07@hotmail.com>
2026-02-19 16:31:35 +1000

    Update math.rs

--- programs/aaped-launch/src/math.rs
+++ programs/aaped-launch/src/math.rs
@@ -29,7 +29,31 @@ pub fn ceil_div(a: u128, b: u128) -> Result<u128> {
             b.checked_sub(1)
                 .ok_or(error!(AapedError::MathOverflow))?,
         )
-        .ok_or(error!(AapedError::MathOverflow))?
+        .ok_or(error!(AapedErropub fn quote_buy(
+    sol_in: u128,
+    sol_real: u128,   // use st.sol_collected (net curve progression)
+    tok_real: u128,   // sale vault remaining
+    fee_total_bps: u128,
+    fee_lp_bps: u128,
+) -> Result<(u128, u128, u128)> {
+    require!(sol_in > 0, AapedError::InvalidAmount);
+
+    let base_fee = bps_amount(sol_in, fee_total_bps)?;
+    let sol_eff = sol_in
+        .checked_sub(base_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    // Curve output on NET (fees handled outside)
+    let (tokens_out, _, _) = curve_buy(sol_eff, sol_real, tok_real, 0)?;
+    require!(tokens_out > 0, AapedError::ZeroOutput);
+
+    let lp_fee = bps_amount(sol_in, fee_lp_bps)?;
+    let total_fee = base_fee
+        .checked_add(lp_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    Ok((tokens_out, total_fee, lp_fee))
+        }r::MathOverflow))?
         .checked_div(b)
         .ok_or(error!(AapedError::MathOverflow))?)
 }
@@ -162,22 +186,54 @@ pub fn curve_sol_eff_for_exact_tokens_cp(
 
 pub fn quote_buy(
     sol_in: u128,
-    sol_real: u128,
-    tok_real: u128,
+    sol_real: u128,   // use st.sol_collected (net curve progression)
+    tok_real: u128,   // sale vault remaining
     fee_total_bps: u128,
     fee_lp_bps: u128,
 ) -> Result<(u128, u128, u128)> {
-    // returns:
-    // (tokens_out, total_fee, lp_fee)
+    require!(sol_in > 0, AapedError::InvalidAmount);
+
+    let base_fee = bps_amount(sol_in, fee_total_bps)?;
+    let sol_eff = sol_in
+        .checked_sub(base_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    // Curve output on NET (fees handled outside)
+    let (tokens_out, _, _) = curve_buy(sol_eff, sol_real, tok_real, 0)?;
+    require!(tokens_out > 0, AapedError::ZeroOutput);
+
+    let lp_fee = bps_amount(sol_in, fee_lp_bps)?;
+    let total_fee = base_fee
+        .checked_add(lp_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    Ok((tokens_out, total_fee, lp_fee))
 }
 
 pub fn quote_sell(
     tokens_in: u128,
-    sol_real: u128,
-    tok_real: u128,
+    sol_real: u128,   // treasury real reserve (treasury lamports - lp bucket)
+    tok_real: u128,   // sale vault amount
     fee_total_bps: u128,
     fee_lp_bps: u128,
 ) -> Result<(u128, u128, u128)> {
-    // returns:
-    // (sol_out_net, total_fee, lp_fee)
+    require!(tokens_in > 0, AapedError::InvalidAmount);
+
+    let sol_gross = curve_sell_gross(tokens_in, sol_real, tok_real)?;
+    require!(sol_gross > 0, AapedError::ZeroOutput);
+
+    let base_fee = bps_amount(sol_gross, fee_total_bps)?;
+    let lp_fee = bps_amount(sol_gross, fee_lp_bps)?;
+
+    let total_fee = base_fee
+        .checked_add(lp_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    let sol_out_net = sol_gross
+        .checked_sub(base_fee)
+        .ok_or(error!(AapedError::MathOverflow))?
+        .checked_sub(lp_fee)
+        .ok_or(error!(AapedError::MathOverflow))?;
+
+    Ok((sol_out_net, total_fee, lp_fee))
 }
