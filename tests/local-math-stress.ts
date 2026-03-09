122119164d08aa4f7f3e67383fb7a152a20f085e
detox267 <shanaman07@hotmail.com>
2026-03-09 12:56:07 +1000

    Update local-math-stress.ts

--- tests/local-math-stress.ts
+++ tests/local-math-stress.ts
@@ -28,11 +28,11 @@ const TOTAL_SUPPLY = new anchor.BN("1000000000000000"); // 1,000,000,000 * 1e6
 const SALE_SUPPLY = new anchor.BN("820000000000000");   // 820,000,000 * 1e6
 const LP_SUPPLY = new anchor.BN("180000000000000");     // 180,000,000 * 1e6
 
-const CURVE_BUY_LAMPORTS = new anchor.BN((2 * LAMPORTS_PER_SOL).toString());
-const AMM_BUY_LAMPORTS = new anchor.BN((0.6 * LAMPORTS_PER_SOL).toString());
+const CURVE_BUY_LAMPORTS = new anchor.BN((15 * LAMPORTS_PER_SOL).toString());
+const AMM_BUY_LAMPORTS = new anchor.BN((10 * LAMPORTS_PER_SOL).toString());
 
-const CURVE_MAX_BUYS = 1000;
-const AMM_BUY_COUNT = 100;
+const CURVE_MAX_BUYS = 30;
+const AMM_BUY_COUNT = 1000;
 const AMM_SELL_COUNT = 50;
 const AMM_LIVE_STATE = 3;
 
