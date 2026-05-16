027b5be00e38e928ca9769630a20266665295483
detox267 <shanaman07@hotmail.com>
2026-05-17 07:28:40 +1000

    Update math.rs

--- programs/aaped-launch/src/math.rs
+++ programs/aaped-launch/src/math.rs
@@ -4,8 +4,8 @@ use crate::errors::AapedError;
 pub const LAMPORTS_PER_SOL: u128 = 1_000_000_000;
 pub const TOKEN_DECIMALS: u128 = 1_000_000; // 6 decimals
 
-pub const V_SOL: u128 = 69 * LAMPORTS_PER_SOL + 500_000_000; // 34.5 SOL
-pub const V_TOK: u128 = 520_000_000 * TOKEN_DECIMALS;
+pub const V_SOL: u128 = 117 * LAMPORTS_PER_SOL;
+pub const V_TOK: u128 = 760_000_000 * TOKEN_DECIMALS;
 
 #[inline]
 pub fn bps_amount(amount: u128, bps: u128) -> Result<u128> {
